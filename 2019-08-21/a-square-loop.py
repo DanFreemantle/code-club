daa899f3ce8534acbaa93362e5a5b0cbf4270326
Dan Freemantle <dan@fatlemon.co.uk>
2019-09-16 21:52:54 +0100

    Implemented a-square-loop.py
    Added code for a-square-loop

--- 2019-08-21/a-square-loop.py
+++ 2019-08-21/a-square-loop.py
@@ -11,4 +11,16 @@ For instance, if the user enters "5" the expected output will be the numbers 0, 
 16
 25
 '''
+square = input("Please enter an integer between 2 and 20: ")
 
+try:
+    maximum = int(square)
+
+    if maximum < 2 or maximum > 20:
+        raise ValueError()
+
+    for i in range(0, maximum + 1):
+        print(i * i)
+
+except ValueError:
+    print("Please enter a valid number in the range 2 to 20")
