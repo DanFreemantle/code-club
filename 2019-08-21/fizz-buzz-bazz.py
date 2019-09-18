961f4807972a2a0b055c16c574c0059ea9096756
Dan Freemantle <dan@fatlemon.co.uk>
2019-09-18 12:53:53 +0100

    FizzBuzzBazz implementation
    Updated FizzBuzzBazz with a working program, the first FizzBuzzBazz is at 105 so you won't see one in the normal range.

--- 2019-08-21/fizz-buzz-bazz.py
+++ 2019-08-21/fizz-buzz-bazz.py
@@ -8,3 +8,20 @@ For multiples of three and seven, print "FizzBazz" (instead of the number)
 For multiples of five and seven, print "BuzzBazz" (instead of the number)
 For multiples of three, five and seven, print "FizzBuzzBazz" (instead of the number)
 '''
+
+for number in range(1, 101):
+    result = ""
+
+    if number % 3 == 0:
+        result = result + "Fizz"
+
+    if number % 5 == 0:
+        result = result + "Buzz"
+
+    if number % 7 == 0:
+        result = result + "Bazz"
+
+    if result == "":
+        result = number
+    
+    print(result)
